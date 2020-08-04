f6dc908be77dcbfd85516631e06777686fcf3d52
horizon-xmguo <horizon-xmguo@users.noreply.github.com>
2020-08-04 22:22:17 +0800

    组件拆分，增加交互，样式分离

---
+++ components/common/HorizontalBar.vue
@@ -0,0 +1,11 @@
+<template>
+	<view style="height: 20rpx; width: 100%; background-color: #f5f5f4;">
+		
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App'
-
+import HorizontalBar from 'components/common/HorizontalBar.vue'
 Vue.config.productionTip = false
-
+Vue.component('HorizontalBar', HorizontalBar)
 App.mpType = 'app'
 
 const app = new Vue({
